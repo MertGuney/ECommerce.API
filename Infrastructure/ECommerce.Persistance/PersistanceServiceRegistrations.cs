dbdf9b2517cf06b7aa082fbb1dc897d9b9157ec6
MertGuney <guneeymert@gmail.com>
2023-01-13 15:48:22 +0300

    Dummy Test Data Added

---
+++ Infrastructure/ECommerce.Persistance/PersistanceServiceRegistrations.cs
@@ -0,0 +1,19 @@
+﻿using ECommerce.Application.Abstractions;
+using ECommerce.Persistance.Concretes;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommerce.Persistance
+{
+    public static class PersistanceServiceRegistrations
+    {
+        public static void AddPersistanceServices(this IServiceCollection services)
+        {
+            services.AddScoped<IProductService, ProductService>();
+        }
+    }
+}
---
+++ Presentation/ECommerce.API/Controllers/ProductsController.cs
@@ -0,0 +1,23 @@
+﻿using ECommerce.Application.Abstractions;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ECommerce.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        private readonly IProductService _productService;
+
+        public ProductsController(IProductService productService)
+        {
+            _productService = productService;
+        }
+
+        public IActionResult GetProducts()
+        {
+            return Ok(_productService.GetProducts());
+        }
+    }
+}
